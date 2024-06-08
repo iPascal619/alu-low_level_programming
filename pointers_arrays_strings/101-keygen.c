3204a841a1d32b18fced217b83a23822257cc4a6
iPascal619 <c.onuoha@alustudent.com>
2024-06-08 09:16:50 -0700

    Attempted Advanced tasks

---
+++ pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ */
+int main(void)
+{
+int pass, sum;
+
+	srand(time(NULL));
+	sum = 0;
+	while (sum <= 2645)
+	{
+		pass = (rand() % 128);
+		sum += pass;
+		printf("%c", pass);
+	}
+	printf("%c", 2772 - sum);
+
+	return (0);
+}
